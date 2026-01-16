111b5411f5cd1628a25bb0b0c99c59d46286afd2
FranciscoEscano <francisco.escano@2itesting.com>
2026-01-16 16:14:57 +0000

    changes basename

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter>
         <ExamProvider>
           <App />
         </ExamProvider>
